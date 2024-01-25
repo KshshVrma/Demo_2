83e363f2d743f06f7bd8f8bf6745bebcf3547516
Kashish <kashishverma8382@gmail.com>
2024-01-25 23:55:20 +0530

    first comment

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import wow.Helper;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String sw=sc.nextLine();
+        String c=new StringBuilder(sw).reverse().toString();
+        Helper one=new Helper(23);
+        one.get();
+        System.out.println(c);
+    }
+}
---
+++ src/wow/Helper.java
@@ -0,0 +1,11 @@
+package wow;
+public class Helper {
+int s;
+public Helper(int k){
+    this.s=k;
+}
+public void get(){
+    System.out.println(this.s);
+}
+
+}
